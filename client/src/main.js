d3f152036e92daf3c20920363739f0efc475f4cf
mahimaarora <201652011@iiitvadodara.ac.in>
2018-04-17 14:53:06 +0530

    updated url

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,7 @@ window.TEXTURES = "bbo_textures";
 startGame();
 
 function startGame() {
-	socket = io("https://explosion123.herokuapp.com");
+	socket = io("https://explosion12.herokuapp.com");
     // socket = io("http://localhost:8000");
 
     require("./game/mods/phaser_enhancements");
